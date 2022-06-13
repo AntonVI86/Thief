48781da0d27199e2895f2a15e3d1f596a461024f
AntonVI86 <lionter0@gmail.com>
2022-06-13 22:50:48 +0300

    Init

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -4,40 +4,28 @@ using UnityEngine.Events;
 
 public class Alarm : MonoBehaviour
 {
-    [SerializeField] private GameObject _door;
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private AudioClip _signalSound;
-    [SerializeField] private UnityEvent _entered;
 
     private float _minVolumeValue = 0;
     private float _maxVolumeValue = 1;
     private float _stepValue = 0.05f;
     private float _step = 0;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void EnterToHouse() 
     {
-        if (collision.TryGetComponent<ThiefMoving>(out ThiefMoving thief)) 
-        {
-            _step = 0;
-            _audioSource.Stop();
-            _audioSource.PlayOneShot(_signalSound);
-            StopAllCoroutines();
-            StartCoroutine(TurnOnSignal());
-            _door.SetActive(false);
-        }
+        _step = 0;
+        _audioSource.Stop();
+        _audioSource.PlayOneShot(_signalSound);
+        StopAllCoroutines();
+        StartCoroutine(TurnOnSignal());
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        _entered?.Invoke();
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
+    public void LeaveHouse() 
     {
         _step = 0;
         StopAllCoroutines();
         StartCoroutine(TurnOffSignal());
-        _door.SetActive(true);
     }
 
     private IEnumerator TurnOnSignal()
---
+++ Assets/Scripts/House.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class House : MonoBehaviour
+{
+    [SerializeField] private GameObject _door;
+    [SerializeField] private UnityEvent _entered;
+    [SerializeField] private UnityEvent _leaved;
+    [SerializeField] private UnityEvent _started;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _entered?.Invoke();
+        _door.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        _started?.Invoke();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _leaved?.Invoke();
+        _door.SetActive(true);
+    }
+}
